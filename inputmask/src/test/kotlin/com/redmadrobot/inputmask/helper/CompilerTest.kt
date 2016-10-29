3730594ad7ed003adcdc149ee74fedda30a288af
Artem Kulakov <ak@redmadrobot.com>
2016-10-29 12:55:35 +0300

    Add tests for FormatSanitizer and Compiler

---
+++ inputmask/src/test/kotlin/com/redmadrobot/inputmask/helper/CompilerTest.kt
@@ -0,0 +1,49 @@
+package com.redmadrobot.inputmask.helper
+
+import com.redmadrobot.inputmask.model.State
+import com.redmadrobot.inputmask.model.state.*
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.util.*
+
+/**
+ * @author: Fi5t
+ */
+class CompilerTest {
+
+    @Test(expected = Compiler.FormatError::class)
+    fun compile_BadMask_ThrowFormatError() {
+        Compiler().compile("[00[9]9]")
+    }
+
+    @Test
+    fun compile_ReturnsCorrectState() {
+        val initialState = Compiler().compile("[09]{.}[09]{.}19[00]")
+
+        var state: State? = initialState
+        val stateList = ArrayList<State>(10)
+        val correctnessList = ArrayList<Boolean>(10)
+
+        while (null != state && state !is EOLState) {
+            stateList.add(state)
+            state = state?.child
+        }
+
+        with(correctnessList) {
+            add(stateList[0] is ValueState)
+            add(stateList[1] is OptionalValueState)
+            add(stateList[2] is FixedState)
+            add(stateList[3] is ValueState)
+            add(stateList[4] is OptionalValueState)
+            add(stateList[5] is FixedState)
+            add(stateList[6] is FreeState)
+            add(stateList[7] is FreeState)
+            add(stateList[8] is ValueState)
+            add(stateList[9] is ValueState)
+        }
+
+        val isCorrectState = correctnessList.reduce { x, y -> x && y }
+
+        assertTrue(isCorrectState)
+    }
+}
---
+++ inputmask/src/test/kotlin/com/redmadrobot/inputmask/helper/FormatSanitizerTest.kt
@@ -0,0 +1,23 @@
+package com.redmadrobot.inputmask.helper
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author: Fi5t
+ */
+class FormatSanitizerTest {
+
+    @Test(expected = Compiler.FormatError::class)
+    fun sanitize_BadMask_ThrowFormatError() {
+        FormatSanitizer().sanitize("+7 ([0[0]0]) [000]-[00]-[00]")
+    }
+
+    @Test
+    fun sanitize_ReturnsSanitizedString() {
+        val expectedString = "[0099]"
+        val actualString = FormatSanitizer().sanitize("[0909]")
+
+        assertEquals(expectedString, actualString)
+    }
+}
