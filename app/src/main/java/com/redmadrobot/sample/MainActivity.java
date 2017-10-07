8ff859f868a0aa82161e9a1ffb89a2005dc5f299
aleien <aleien@yandex-team.ru>
2017-10-07 18:13:09 +0300

    Fix for choosing right mask

--- app/src/main/java/com/redmadrobot/sample/MainActivity.java
+++ app/src/main/java/com/redmadrobot/sample/MainActivity.java
@@ -26,6 +26,12 @@ public final class MainActivity extends Activity {
         final EditText editText = (EditText) findViewById(R.id.edit_text);
         final List<String> affineFormats = new ArrayList<>();
         affineFormats.add("8 ([000]) [000] [000] [00]");
+        affineFormats.add("+1([000])[000]-[0000]");
+        affineFormats.add("+20([000])[000]-[0000]");
+        affineFormats.add("+27-[00]-[000]-[0000]");
+        affineFormats.add("+30([000])[000]-[0000]");
+        affineFormats.add("+31-[00]-[000]-[0000]");
+        affineFormats.add("+49([000])[000]-[0000]");
 
         final MaskedTextChangedListener listener = new PolyMaskTextChangedListener(
             "+7 ([000]) [000] [00] [00]",
